0885f33dab1d09e7c8e099b4367e58828ff22b58
mdlldz <18312315376@qq.com>
2025-09-01 16:13:47 +0800

    first

---
+++ Boolean.java
@@ -0,0 +1,10 @@
+public class Boolean{
+	public static void main (String [] args){
+		boolean ispass = false;
+	if (ispass == false) {
+System.out.println("����");
+}else {
+	System.out.println("����");
+}
+}
+}
---
+++ homework.java
@@ -0,0 +1,15 @@
+public class homework{
+	public static void main(String [] args){
+		char n1 = '\t';
+		char n2 = '\n';
+		char n3 = '1';
+		char n4 = '\r';
+		char n5 = '\\';{
+			System.out.println(n1);
+			System.out.println(n2);
+			System.out.println(n3);
+			System.out.println(n4);
+			System.out.println(n5);
+		}
+	}
+	}
